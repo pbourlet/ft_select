466564980dc00d42b8434303b27d1387e458fe5d
pbourlet <pbourlet@student.42.fr>
2017-06-01 16:19:01 +0200

    FDp

--- ft_color.c
+++ ft_color.c
@@ -6,7 +6,7 @@
 /*   By: pbourlet <pbourlet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/23 15:44:15 by pbourlet          #+#    #+#             */
-/*   Updated: 2017/05/23 17:08:15 by pbourlet         ###   ########.fr       */
+/*   Updated: 2017/06/01 16:16:03 by pbourlet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,28 @@ static void	ft_color_set(struct stat statis)
 {
 	if ((S_ISVTX & statis.st_mode) && (S_IXOTH & statis.st_mode) &&
 			S_IWOTH & statis.st_mode)
-		ft_putstr("\033[42m\033[30m");
+		ft_putstr_fd("\033[42m\033[30m", 0);
 	else if (S_ISDIR(statis.st_mode) && S_IRUSR & statis.st_mode &&
 			S_IWUSR & statis.st_mode && S_IXUSR & statis.st_mode &&
 			S_IRGRP & statis.st_mode && S_IWGRP & statis.st_mode &&
 			S_IXGRP & statis.st_mode && S_IROTH & statis.st_mode &&
 			S_IWOTH & statis.st_mode && S_IXOTH & statis.st_mode)
-		ft_putstr("\033[43m\033[30m");
+		ft_putstr_fd("\033[43m\033[30m",0);
 	else if (S_ISDIR(statis.st_mode))
-		ft_putstr("\033[36;01m");
+		ft_putstr_fd("\033[36;01m", 0);
 	else if (S_ISBLK(statis.st_mode))
-		ft_putstr("\033[46m\033[34m");
+		ft_putstr_fd("\033[46m\033[34m", 0);
 	else if (S_ISCHR(statis.st_mode))
-		ft_putstr("\033[43m\033[34m");
+		ft_putstr_fd("\033[43m\033[34m", 0);
 	else if (S_ISLNK(statis.st_mode))
-		ft_putstr("\033[35m");
+		ft_putstr_fd("\033[35m", 0);
 	else if (S_ISFIFO(statis.st_mode))
-		ft_putstr("\033[33m");
+		ft_putstr_fd("\033[33m", 0);
 	else if (S_ISSOCK(statis.st_mode))
-		ft_putstr("\033[36m");
+		ft_putstr_fd("\033[36m", 0);
 	else if (S_IXOTH & statis.st_mode || S_IXGRP & statis.st_mode
 			|| S_IXUSR & statis.st_mode)
-		ft_putstr("\033[31m");
+		ft_putstr_fd("\033[31m", 0);
 }
 
 void		ft_color(void)
